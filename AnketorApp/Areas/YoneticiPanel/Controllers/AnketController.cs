2cf206516fc2aafaf43d59ba631b014b53eedf41
alpsarikisla <50963484+alpsarikisla@users.noreply.github.com>
2025-01-14 17:39:41 +0300

    .

--- AnketorApp/Areas/YoneticiPanel/Controllers/AnketController.cs
+++ AnketorApp/Areas/YoneticiPanel/Controllers/AnketController.cs
@@ -12,11 +12,11 @@ namespace AnketorApp.Areas.YoneticiPanel.Controllers
     public class AnketController : Controller
     {
         AnketSistemiEntities db = new AnketSistemiEntities();
-        // GET: YoneticiPanel/Anket
-        public ActionResult Index()
+        public ActionResult Liste()
         {
-            return View();
+            return View(db.Anketler.ToList());
         }
+
         [HttpGet]
         public ActionResult Ekle()
         {
@@ -40,6 +40,39 @@ namespace AnketorApp.Areas.YoneticiPanel.Controllers
                     ViewBag.HataMesaj = "Anket eklenirken hata oluştu"; 
                 }
                 
+            }
+            return View(model);
+        }
+        [HttpGet]
+        public ActionResult Duzenle(int? id)
+        {
+            if (id != null)
+            {
+                Anketler model = db.Anketler.Find(id);
+                if (model != null)
+                {
+                    return View(model);
+                }
+            }
+            
+            return RedirectToAction("Liste","Anket");
+        }
+        [HttpPost]
+        public ActionResult Duzenle(Anketler model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Entry(model).State = System.Data.Entity.EntityState.Modified;
+                    //Bu sınıfın içindeki verilere göre veritabanında güncelleme işlemi yapılması için geçerli komut
+                    db.SaveChanges();
+                    ViewBag.Basarili = "Anket Düzenlendi";
+                }
+                catch
+                {
+                    ViewBag.HataMesaj = "Düzenlenirken Bir Hata Oluştu";
+                }
             }
             return View(model);
         }
